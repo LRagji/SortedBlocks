2222b149056f8e207df4b7abdefd4e8cdbdc48bc
LaukikOnNMac <22937426+LRagji@users.noreply.github.com>
2022-11-22 17:01:50 +0530

    Ground Zero

--- source/i-append-store.ts
+++
@@ -1,7 +0,0 @@
-export interface IAppendStore {
-    Id: string;
-    OptimalBufferSize: number | undefined
-    append(data: Buffer): void;
-    read(position: number, utf8CharactersToRead: number): string;
-    fetchCurrentSize(): number;
-}
